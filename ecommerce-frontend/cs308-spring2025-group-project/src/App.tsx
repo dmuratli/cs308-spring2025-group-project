215329bfcfbb76f2fad340dfa4e695ba36c8d908
nidakayaduman <elifnida@gmail.com>
2025-03-06 00:36:03 +0300

    Adding the HomePage Route

--- ecommerce-frontend/cs308-spring2025-group-project/src/App.tsx
+++ ecommerce-frontend/cs308-spring2025-group-project/src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import React from "react";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage"; // Import the HomePage component
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Add the HomePage Route */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
