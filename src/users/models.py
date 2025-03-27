00e91156d2851e98e6bf798c7a034066f3650ff3
Teoman Arabul <119057447+teo123man@users.noreply.github.com>
2025-03-28 02:11:54 +0300

    Update models.py
    just one unnecessarily added row of text is gone, nothing of importance

--- src/users/models.py
+++ src/users/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-from admin_panel.models import Product
-
-
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     groups = models.ManyToManyField(
